e7a89c8260e9b8907089bde81f162642a346f1ea
RaphaelTurmel <serviceturmel1@gmail.com>
2025-07-24 11:34:51 -0400

    Delete src directory

--- src/routes/dashboard/employees/new/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import EmployeeForm from '../EmployeeForm.svelte';
-    export let form;
-</script>
-
-<div class="p-4">
-    <h1 class="text-2xl font-bold mb-4">Add New Employee</h1>
-    <EmployeeForm {form} />
-</div>
